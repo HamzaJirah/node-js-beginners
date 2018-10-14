ed0acb498b9a5339a33140456117ee254ff30dd6
HamzaJirah <hamzajirah@gmail.com>
2018-10-14 06:34:04 +0100

    write html

--- app.js
+++ app.js
@@ -1,23 +1,25 @@
 const http = require ('http');
 const fs = require ('fs');
+// create host
+const hostName = '127.0.0.1';
+const port = 3000;
 
 fs.readFile('index.html', (err, html) => {
   if(err){
     throw err;
   }
+
+    // create server
+  const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-type', 'text/plain');
+    res.write(html);
+    res.end();
+  })
+
+  server.listen(port, hostName, ()=>{
+    console.log('server started on port ' + port)
+  })
 })
 
-// create host
-const hostName = '127.0.0.1';
-const port = 3000;
 
-// create server
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-type', 'text/plain');
-  res.end('Hello World');
-})
-
-server.listen(port, hostName, ()=>{
-  console.log('server started on port ' + port)
-})
