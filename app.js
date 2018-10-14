c72a4b102e1c48a3fffb148ea735f8ad38c981cc
HamzaJirah <hamzajirah@gmail.com>
2018-10-14 06:19:02 +0100

    output plain text

--- app.js
+++ app.js
@@ -7,4 +7,6 @@ const port = 3000;
 // create server
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
+  res.setHeader('Content-type', 'text/plain');
+  res.end('Hello World');
 })
